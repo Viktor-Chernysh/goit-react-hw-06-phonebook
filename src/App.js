0a90eda56c8d069907d5970d95ceb060d3e85c88
Vitkor Chernysh <viktor.chernysh.work@gmail.com>
2021-11-14 16:52:01 +0200

    redux toolkit

--- src/App.js
+++ src/App.js
@@ -7,13 +7,6 @@ import Contacts from './components/Contacts/Contacts';
 import { connect } from 'react-redux';
 // import store from './redux/store';
 
-// const contactsArr = [
-//   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-//   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-//   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-//   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-// ];
-
 function App({ contacts }) {
   return (
     <>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,44 @@
-import { createStore, combineReducers } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-
+// import {createStore , combineReducers } from 'redux';
+// import { composeWithDevTools } from 'redux-devtools-extension';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { contactReducer } from './contacts/counter-reducers';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+// const rootReducer = combineReducers({
+//   contacts: contactReducer,
+// });
+
+const persistConfig = {
+  key: 'contacts',
+  storage,
+  blacklist: ['filter'],
+};
+
 const rootReducer = combineReducers({
-  contacts: contactReducer,
+  contacts: persistReducer(persistConfig, contactReducer),
 });
 
-const store = createStore(rootReducer, composeWithDevTools());
-export default store;
+const middleware = getDefaultMiddleware =>
+  getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  });
+
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware,
+});
+
+export const persistor = persistStore(store);
+// const store = createStore(rootReducer, composeWithDevTools());
